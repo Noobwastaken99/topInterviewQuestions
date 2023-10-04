22c281f819347a21b894c94ac6f6e1337fe40da7
Noobwastaken99 <czmoni99@yahoo.com>
2023-10-04 17:55:31 -0500

    rmfsa moved to finished folder

--- finishedEasy/removeDuplicatesFromSortedArray.java
+++ finishedEasy/removeDuplicatesFromSortedArray.java
@@ -1,3 +1,4 @@
+package finishedEasy;
 /* Question 26. Remove Duplicates from Sorted Array (easy)
 Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. 
 The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
